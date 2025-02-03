1cda2755c59cef2ef1e3ff47b1692768912edbd6
Anurag <anuragsamal123@gmail.com>
2025-02-03 11:42:12 +0530

    tried3

--- src/Components/FirstPage/FirstPage.jsx
+++ src/Components/FirstPage/FirstPage.jsx
@@ -3,8 +3,13 @@ import './FirstPage.css'
 
 const FirstPage = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center ' >
-        <div className=' heroimg h-[95%] w-[97%] rounded-3xl bg-[url(Images/heroimg.jpg)] bg-cover bg-center flex flex-col gap-[3%] justify-center items-center ' >
+    <div className='h-screen w-full flex justify-center items-center relative object-cover object-center overflow-hidden ' >
+      <div  className='h-[95%] w-[97%] rounded-3xl fimg object-cover object-center overflow-hidden ' >
+      <img className='h-full w-full '  src="Images/heroimg.jpg" alt="" />
+      </div>
+      
+
+        <div className='absolute heroimg h-[95%] w-[97%] rounded-3xl bg-cover bg-center flex flex-col gap-[3%] justify-center items-center ' >
             <p className=' herotext1 text-6xl text-white ' >Luxury Oral Care. Wherever You Are.</p>
             <p className=' herotext2 text-4xl text-white ' >From a Legacy of Award-Winning Excellence.</p>
             <div className='flex flex-col justify-center items-center' >
